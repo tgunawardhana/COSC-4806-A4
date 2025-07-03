814271016c184116ec17a35f11db17dfe9cde6de
tgunawardhana <tgunawardhana@algomau.ca>
2025-07-03 18:05:52 +0000

    Add functions to insert, update and delete reminders

--- app/models/Reminder.php
+++ app/models/Reminder.php
@@ -11,14 +11,33 @@ class Reminder {
     $statement = $dbh->prepare("select * from reminders;");
     $statement->execute();
     $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+    return $rows;
   }
 
-  public function update_reminder($reminder_id){
+  public function insert_reminder($subject, $user_id) {
     $dbh = db_connect();
-    $statement = $dbh->prepare("update reminders set reminder = :reminder where id = :id;");
-    $statement->bindParam(':reminder', $reminder);
+    $statement = $dbh->prepare("insert into reminders (subject, user_id) values (:subject, :user_id);");
+    $statement->bindParam(':subject', $subject);
+    $statement->bindParam(':user_id', $user_id);
+    $statement->execute();
+    header("location: /reminders/index");
+  }
+
+  public function update_reminder($id, $subject, $user_id){
+    $dbh = db_connect();
+    $statement = $dbh->prepare("update reminders set subject = :subject, user_id = :user_id where id = :id;");
+    $statement->bindParam(':subject', $subject);
+    $statement->bindParam(':user_id', $user_id);
+    $statement->bindParam(':id', $id);
+    $statement->execute();
+    header("location: /reminders/index");
+  }
+
+  public function delete_reminder($id){
+    $dbh = db_connect();
+    $statement = $dbh->prepare("delete from reminders where id = :id;");
     $statement->bindParam(':id', $id);
     $statement->execute();
   }
-
+  
 }
