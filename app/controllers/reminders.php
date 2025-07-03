5449e3762df4cb0915415a215639ff72ebe522a8
tgunawardhana <tgunawardhana@algomau.ca>
2025-07-03 18:07:00 +0000

    Add functions to call Reminder model functions

--- app/controllers/reminders.php
+++ app/controllers/reminders.php
@@ -3,7 +3,36 @@
 class Reminders extends Controller {
 
     public function index() {
-      $this->view('reminders/index');
+
+      $reminder = $this->model('Reminder');
+      $list_of_reminders = $reminder->get_all_reminders();
+
+      
+      $this->view('reminders/index', ['reminders' =>$list_of_reminders]);
     }
 
+    public function create(){
+
+      $this->view('reminders/create');
+    }
+
+    public function insert(){
+      $subject = $_REQUEST['subject'];
+      $user_id = $_SESSION['user_id'];
+
+      $reminder = $this->model('Reminder');
+      $reminder->insert_reminder($subject, $user_id);
+    }
+  
+    public function update($id){
+      $subject = $_REQUEST['subject'];
+      $user_id = $_SESSION['user_id'];
+      $reminder = $this->model('Reminder');
+      $reminder->update_reminder($id, $subject, $user_id);
+    }
+
+   public function delete($id){
+     $reminder = $this->model('Reminder');
+     $reminder->delete_reminder($id);
+   }
 }
