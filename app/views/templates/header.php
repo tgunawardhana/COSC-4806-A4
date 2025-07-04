db356560b49a56c737cfea4d37863d895e77a714
tgunawardhana <tgunawardhana@algomau.ca>
2025-07-04 04:18:00 +0000

    Updated nav bar link to go to home

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -17,7 +17,7 @@ if (!isset($_SESSION['auth'])) {
     <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">COSC 4806</a>
+    <a class="navbar-brand" href="/home">COSC 4806</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
