00ac684562615c3355d9d9ef7a6cbcb3bc7d6155
tgunawardhana <tgunawardhana@algomau.ca>
2025-07-04 18:04:51 +0000

    Add form to display completed checkbox and update the table on post

--- app/views/reminders/index.php
+++ app/views/reminders/index.php
@@ -18,20 +18,31 @@
                 <tr>
                   <th>Subject</th>
                   <th>Time Stamp</th>
+                  <th>Completed</th>
                   <th>Update</th>
                   <th>Delete</th>
                 </tr>
               </thead>
               <tbody>
+               
                 <?php foreach ($data['reminders'] as $reminder): ?>
                   <tr>
                     <td> <?php echo $reminder['subject'] ?></td>
                     <td> <?php echo $reminder['created_at'] ?> </td>
+                    <td class="text-center">
+
+                      <form method="post" action="/reminders/updateStatus" style="display:inline;">
+                      <input type="hidden" name="id" value="<?php echo $reminder['id']; ?>">
+                      
+                      <input type="checkbox" name="completed" <?php echo $reminder['completed'] ? 'checked' : '' ?> onChange="this.form.submit()"> 
+  </form>
+  
+                    </td>
                     <td> 
                       <form method="post" action="/reminders/edit" style="display:inline;">
                         <input type="hidden" name="id" value="<?php echo $reminder['id']; ?>">
                         <input type="hidden" name="sub" value="<?php echo $reminder['subject']; ?>">
-                        <button type="submit">Update</button>
+                        <button class="btn-secondary" type="submit">Update</button>
                       </form>
 
                     
@@ -40,7 +51,7 @@
                       
                       <form method="post" action="/reminders/delete" style="display:inline;">
                         <input type="hidden" name="id" value="<?php echo $reminder['id']; ?>">
-                        <button type="submit">Delete</button>
+                        <button type="submit" class="btn-danger">Delete</button>
                       </form>
 
                       
